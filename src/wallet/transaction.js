37dc82302cd47e8ece2e12d86eac7c9ea1bd7f7b
Ross Sheppard <ross.sheppard@a000889.attlocal.net>
2020-11-06 12:47:08 -0600

    Updated Transaction and tests
     Committer: Ross Sheppard <ross.sheppard@fusion.dev>

--- src/wallet/transaction.js
+++ src/wallet/transaction.js
@@ -4,6 +4,16 @@ module.exports = class Transaction {
   constructor({ amount, recipient, senderWallet }) {
     this.id = uuid.v1();
     this.outputMap = this.createOutputMap({ amount, recipient, senderWallet });
+    this.input = this.createInput({ outputMap: this.outputMap , senderWallet });
+  }
+
+  createInput({ outputMap, senderWallet }) {
+    return {
+      address: senderWallet.publicKey,
+      amount: senderWallet.balance,
+      signature: senderWallet.sign(outputMap),
+      timestamp: Date.now(),
+    };
   }
 
   createOutputMap({ amount, recipient, senderWallet }) {
--- src/wallet/transaction.test.js
+++ src/wallet/transaction.test.js
@@ -1,5 +1,6 @@
 const Transaction = require('./Transaction');
 const Wallet = require('.');
+const { verifySignature } = require('../util');
 
 describe('Transaction', () => {
   let amount;
@@ -30,6 +31,34 @@ describe('Transaction', () => {
     it('outputs the remaining balance for `senderWallet`', () => {
       expect(transaction.outputMap[senderWallet.publicKey])
         .toEqual(senderWallet.balance - amount);
+    });
+  });
+
+  describe('input', () => {
+    it('has an `input`', () => {
+      expect(transaction).toHaveProperty('input');
+    });
+
+    it('has a `timestamp` in the input', () => {
+      expect(transaction.input).toHaveProperty('timestamp');
+    });
+
+    it('sets the `amount` to the `senderWallet` balance', () => {
+      expect(transaction.input.amount).toEqual(senderWallet.balance);
+    });
+
+    it('sets the `address` to the `senderWallet` publicKey', () => {
+      expect(transaction.input.address).toEqual(senderWallet.publicKey);
+    });
+
+    it('signs the input', () => {
+      const isVerified = verifySignature({
+        data: transaction.outputMap,
+        publicKey: senderWallet.publicKey,
+        signature: transaction.input.signature,
+      });
+
+      expect(isVerified).toBe(true);
     });
   });
 });
